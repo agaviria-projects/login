45b3c7d1f545f37b1c37965d105b20d4c82a957e
Alejandro <agaviria1408@gmail.com>
2025-05-04 14:21:04 -0500

    Se realizan cambios

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,17 @@ import { useState } from "react";
 
 function App() {
   let [isAuthenticated, setIsAuthenticated] = useState(false);
+  let handleLogin =()=>{
+     setIsAuthenticated(true);//token de autorizacion
+  };
 
 
   return (
     <>
       <Routes>
-        <Route path="/Login" element={ isAuthenticated ? <Navigate to="/Dashboard"/> : <Login/>}/>
+        <Route path="/login" element={ isAuthenticated ? <Navigate to="/Dashboard"/> : <Login/>}/>
         <Route path="/Register" element={ isAuthenticated ? <Navigate to="/Dashboard"/> : <Register/>}/>
-        <Route path="/Dashboard" element={ isAuthenticated ? <Dashboard/> : <Navigate to="/Login"/> }/>
+        <Route path="/Dashboard" element={ isAuthenticated ? <Dashboard/> : <Navigate to="/login"/> }/>
         <Route path="*" element={<Login/>}/>
       </Routes>
     </>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>
-        Login
-    </div>
+    <>
+        <div className="container" style={{ marginTop: '20px' }}>
+            <div className="row">
+                <div className="col-sm-8"></div>
+                    <h1 className='col text-center text-primary'>Formulario de Ingreso</h1>
+                </div>    
+        </div>
+    </>
   )
 }
