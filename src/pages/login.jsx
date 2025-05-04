cfb1d32f22876a2d2d8989fe284bef239aec8b18
Alejandro <agaviria1408@gmail.com>
2025-05-04 14:39:41 -0500

    Se crea configuraciones para el formualrio

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
 
 export default function Login() {
+    let {register, handleSubmit} = useForm();
   return (
     <div className="container mt-4">
       <div className="row justify-content-center">
@@ -8,15 +11,15 @@ export default function Login() {
           <h1 className="text-primary my-5">Formulario de Ingreso</h1>
         </div>
             <div className="col-sm-8 ">
-            <form>
+            <form onSubmit={handleSubmit()} className='bg-light-subtle'>
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <label for="exampleInputEmail1" className="form-label">Usuario</label>
+                        <input type="email"{...register("correo",{required: true})} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
                         <label for="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                        <input type="password" {...register("contrasena",{required: true})} className="form-control" id="exampleInputPassword1"/>
                     </div>
                     <div className="mb-3 form-check">
                         <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
