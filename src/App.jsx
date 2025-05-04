af71a6534c5e8cc4fad2ff23d1f06d4fcf86357f
Alejandro <agaviria1408@gmail.com>
2025-05-04 12:20:12 -0500

    Se crea la funcion Navigate para el Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Dashboard from "./pages/dashboard";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  let [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  
+
   return (
     <>
       <Routes>
-        <Route path="/Login" element={<Login/>}/>
-        <Route path="/Register" element={<Register/>}/>
-        <Route path="/Dashboard" element={<Dashboard/>}/>
+        <Route path="/Login" element={ isAuthenticated ? <Navigate to="/Dashboard"/> : <Login/>}/>
+        <Route path="/Register" element={ isAuthenticated ? <Navigate to="/Dashboard"/> : <Register/>}/>
+        <Route path="/Dashboard" element={ isAuthenticated ? <Dashboard/> : <Navigate to="/Login"/> }/>
         <Route path="*" element={<Login/>}/>
       </Routes>
     </>
