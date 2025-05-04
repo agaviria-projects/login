716cf22b262ad74a318ec4952dcca22317edb2a0
Alejandro <agaviria1408@gmail.com>
2025-05-04 14:30:30 -0500

    Se crea el formulario

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 export default function Login() {
   return (
-    <>
-        <div className="container" style={{ marginTop: '20px' }}>
-            <div className="row">
-                <div className="col-sm-8"></div>
-                    <h1 className='col text-center text-primary'>Formulario de Ingreso</h1>
-                </div>    
+    <div className="container mt-4">
+      <div className="row justify-content-center">
+        <div className="col-sm-8 text-center">
+          <h1 className="text-primary my-5">Formulario de Ingreso</h1>
         </div>
-    </>
-  )
+            <div className="col-sm-8 ">
+            <form>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+            </div>    
+      </div>
+    </div>
+  );
 }
+
